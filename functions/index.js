fa92fe010765050b4cbf0316404a97923256da22
Ammar Jussa <ammar26497@gmail.com>
2020-11-28 17:07:25 +0500

    Failed payment integration

---
+++ functions/index.js
@@ -0,0 +1,34 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const { request } = require('express');
+
+const stripe = require('stripe')(
+  'sk_test_51HsQDhBfjYOY3P1kJIUEd2kGBqxkuDApOfaCESUGuXUdKyVi43ufKk0vNvQMG2vwYREEhjyNCd4Ub9D1N0uB8FH500hkbryDCI'
+);
+
+const app = express();
+
+app.use(cors);
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.status(200).send('Hello World buitch'));
+
+app.post('/payments/create', async (req, res) => {
+  console.log(req);
+  const total = req.query.total;
+
+  console.log('Payment Request Received BOOM', total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: 'usd',
+  });
+
+  res.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+exports.api = functions.https.onRequest(app);
--- src/components/Payment/index.js
+++ src/components/Payment/index.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.scss';
 import { useStateValue } from '../../Providers/StateProvider';
 import CheckoutProduct from '../Checkout/CheckoutProduct';
 import FlipMove from 'react-flip-move';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import CurrencyFormat from 'react-currency-format';
+import { getBasketTotal } from '../../Reducers/getBasketTotal';
+import axios from '../../axios';
+
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
 const Payment = () => {
   const [{ basket, user }, dispatch] = useStateValue();
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState(null);
+  const [clientSecret, setClientSecret] = useState(true);
+
+  const stripe = useStripe();
+  const elements = useElements();
+  const history = useHistory();
+
+  useEffect(() => {
+    const getClientSecret = async () => {
+      const response = await axios({
+        method: 'post',
+        url: `/payments/create?total=${5000}`,
+      });
+
+      console.log(response);
+
+      setClientSecret(response.data.clientSecret);
+    };
+
+    getClientSecret();
+  }, [basket]);
+
+  console.log('The secret is ' + clientSecret);
+
+  const handleChange = (e) => {
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : '');
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      })
+      .then(({ paymentIntent }) => {
+        setSucceeded(true);
+        setError(null);
+        setProcessing(false);
+
+        history.replace('/orders');
+      });
+  };
+
   return (
     <div className="payment">
       <div className="container">
@@ -37,7 +93,28 @@ const Payment = () => {
           <div className="title">
             <h3>Payment Methods</h3>
           </div>
-          <div className="details"></div>
+          <div className="details">
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+
+              <div className="price_container">
+                <CurrencyFormat
+                  renderText={(value) => <h3>Order Total: {value}</h3>}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)}
+                  displayType={'text'}
+                  thousandSeparator={true}
+                  prefix={'$'}
+                />
+
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : 'Buy Now'}</span>
+                </button>
+              </div>
+
+              {error && <div>{error}</div>}
+            </form>
+          </div>
         </div>
       </div>
     </div>
