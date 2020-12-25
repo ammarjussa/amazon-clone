c27fce0c17e934b9cfb41f5d99e6b0441abb8d4c
Ammar Jussa <ammar26497@gmail.com>
2020-12-25 14:01:42 +0500

    Removed stripe stuff

--- src/pages/PaymentPage/index.js
+++ src/pages/PaymentPage/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Payment from '../../components/Payment';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-
-const promise = loadStripe(
-  'pk_test_51HsQDhBfjYOY3P1kIWikOScCJSyBjW6wBXnzIDSLdNNdMoMcqgIYUTtnRpFfmW9CRhTtbkojJcX6Bxz557PqH0sa0091MF4w5i'
-);
 
 const PaymentPage = () => {
   return (
     <div>
       <Header />
-      <Elements stripe={promise}>
-        <Payment />
-      </Elements>
+      <Payment />
     </div>
   );
 };
